9094a28f411e254d71427528c9b988013702a424
Tyler <21267024+h5p9sl@users.noreply.github.com>
2020-01-25 18:55:25 -0700

    Fix typos
    Not everyone can type perfect code on their first try

--- HiMe!.py
+++ HiMe!.py
@@ -2,7 +2,7 @@
 import time
 
 def get_password():
-    password = input('Please enter password')
+    password = input('Please enter password: ')
     if password != '123':
         print('Invalid password!')
         return False
@@ -10,15 +10,17 @@ def get_password():
     return True
 
 def get_name():
-    identity = input('Who is this?')
-    return identity == 'JP':
+    identity = input('Who is this? ')
+    return identity == 'JP'
 
 while not get_password():
     # Sleep for 3 seconds so the user can't bruteforce this high security application
+    print('Waiting for 3 seconds...')
     time.sleep(3)
 
 while not get_name():
     # Sleep for 1.69420 seconds to avoid computer-based time attacks
+    print('Waiting for 1.69420 seconds...')
     time.sleep(1.69420)
         
 print("Hello JP!")
