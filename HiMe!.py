636bb1618ebe913e14aef48592c2b05a82ad6144
Tyler <21267024+h5p9sl@users.noreply.github.com>
2020-01-25 18:50:07 -0700

    Implement anti-bruteforce
    This commit simply creates a higher security environment in which the user cannot exploit the login process by brute forcing all of the possible combinations of usernames. I truly believe this code should be examined with great virtue and learned from as I am a coding god.
    
    \- h5p9sl

--- HiMe!.py
+++ HiMe!.py
@@ -1,17 +1,25 @@
-while True:
-    print("Please enter password.")
-    password = input()
-    if password != "123":
-        print("Invalid password!")
-        continue
-    print("Access granted.")
-    break
-while True:
-    print("Who is this?")
-    ident = input()
-    if ident != "JP":
-        continue
-    else:
-        break
+# For time.sleep()
+import time
+
+def get_password():
+    password = input('Please enter password')
+    if password != '123':
+        print('Invalid password!')
+        return False
+    print('Access granted.')
+    return True
+
+def get_name():
+    identity = input('Who is this?')
+    return identity == 'JP':
+
+while not get_password():
+    # Sleep for 3 seconds so the user can't bruteforce this high security application
+    time.sleep(3)
+
+while not get_name():
+    # Sleep for 1.69420 seconds to avoid computer-based time attacks
+    time.sleep(1.69420)
+        
 print("Hello JP!")
     
