9ecffae8e48a6d909fffad6922ff0444cd3db3a3
jprik <60302563+jprik@users.noreply.github.com>
2020-01-26 21:53:58 -0700

    Added gameloop

--- rps.py
+++ rps.py
@@ -1,45 +1,59 @@
-import sys, random
-from random import randint
+import random
 
-def scoreKeeper():
-    scoreKeeper.ties = 0
-    scoreKeeper.wins = 0
-    scoreKeeper.losses = 0
+def gameState():
 
-def aiTurn():
-    
-    aiChoiceInt = random.randint(1, 3)
-    if aiChoiceInt == 1:
-        aiTurn.aiChoiceStr = 'Rock'
-    elif aiChoiceInt == 2:
-        aiTurn.aiChoiceStr = 'Paper'
-    elif aiChoiceInt == 3:
-        aiTurn.aiChoiceStr = 'Scissors'
+    def scoreKeeper():
+        scoreKeeper.ties = 0
+        scoreKeeper.wins = 0
+        scoreKeeper.losses = 0
 
-def isWinner(playerChoice, aiChoice):
-    if playerChoice == 'Rock' and aiChoice == 'Paper':
-        return True
-    elif playerChoice == 'Paper' and aiChoice == 'Rock':
-        return True
-    elif playerChoice == 'Scissors' and aiChoice == 'Paper':
-        return True
-    return False
-        
-def playerTurn():
-    print('Your move!')
-    playerChoiceStr = input()
-    print('Ai Chose {}'.format(aiTurn.aiChoiceStr))
-    
-    if playerChoiceStr == aiTurn.aiChoiceStr:
-        print('Tie game!')
-    elif playerChoiceStr != aiTurn.aiChoiceStr:
-        value = isWinner(playerChoiceStr, aiTurn.aiChoiceStr)
-        if value == True:
-            print('Congratulations, you won!')
-        else:
-            print('Better luck next time!')
-        
-aiTurn()        
-playerTurn()
 
-        
+    def aiTurn():
+
+        aiChoiceInt = random.randint(1, 3)
+        if aiChoiceInt == 1:
+            aiTurn.aiChoiceStr = 'Rock'
+        elif aiChoiceInt == 2:
+            aiTurn.aiChoiceStr = 'Paper'
+        elif aiChoiceInt == 3:
+            aiTurn.aiChoiceStr = 'Scissors'
+
+
+    def isWinner(playerChoice, aiChoice):
+
+        if playerChoice == 'Rock' and aiChoice == 'Paper':
+            return False
+
+        elif playerChoice == 'Paper' and aiChoice == 'Rock':
+            return True
+
+        elif playerChoice == 'Scissors' and aiChoice == 'Paper':
+            return True
+
+        elif playerChoice == 'Scissors' and aiChoice == 'Rock':
+            return False
+
+    def playerTurn():
+        print('Your move!')
+
+        playerChoiceStr = input()
+
+        print('AI Chose {}'.format(aiTurn.aiChoiceStr))
+
+        if playerChoiceStr == aiTurn.aiChoiceStr:
+            print('Tie game!')
+
+        elif playerChoiceStr != aiTurn.aiChoiceStr:
+            value = isWinner(playerChoiceStr, aiTurn.aiChoiceStr)
+
+            if value is True:
+                print('Congratulations, you have won!')
+                gameState()
+
+            else:
+                print('Better luck next time!')
+                gameState()
+
+    aiTurn()
+    playerTurn()
+gameState()
